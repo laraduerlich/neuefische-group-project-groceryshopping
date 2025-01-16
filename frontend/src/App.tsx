141790f1a590df9f108b1119f077b881ba6765af
laraduerlich <lara.duerlich@t-online.com>
2025-01-16 14:44:02 +0100

    chore: frontend setup (tailwind, pages, routes)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,25 @@
 
 import './App.css'
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.tsx";
+import {Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
+import ShoppingListPage from "./pages/ShoppingListPage.tsx";
+import ShoppingPage from "./pages/ShoppingPage.tsx";
 
 function App() {
 
 
   return (
     <>
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+        <Route path={"/shoppinglist"} element={<ShoppingListPage />} />
+        <Route path={"/shopping"} element={<ShoppingPage />} />
+      </Routes>
 
+      <Footer />
     </>
   )
 }
