12338f8245f0b7a5531c85282f2c2bb58958a38a
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-01-19 22:34:01 +0100

    feat: Use tailwind css

--- frontend/src/components/ItemForm.tsx
+++ frontend/src/components/ItemForm.tsx
@@ -22,31 +22,38 @@ export default function ItemForm({onSubmit}: ItemFormProps){
     const sections: Section[] = ["fruit", "vegetable", "dairy", "meat", "bakery", "beverages", "frozen", "snacks", "pantry", "household", "personal care", "other"];
 
     return (
-        <>
-           <h2>Add a new item:</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name:</label>
-                <input type="text"
-                       id="name"
-                       name="name"
-                       value={name}
-                       onChange={(e) => setName(e.target.value)} />
-                <label htmlFor="quantity">Quantity:</label>
-                <input type="text"
-                       id="quantity"
-                       name="quantity"
-                       value={quantity}
-                       onChange={(e) => setQuantity(e.target.value)}  />
-                <label htmlFor="sector">Sector:</label>
-                <select id="sector"
-                       name="sector"
-                       value={section}
-                       onChange={(e) => setSection(e.target.value as Section)}>
+        <form onSubmit={handleSubmit} className="space-y-4">
+            <fieldset className="w-full p-4 space-y-4 border border-gray-300 rounded-md dark:text-gray-800">
+                <legend className="text-2xl font-bold">Add a new item:</legend>
+                <div className="relative rounded-md border border-gray-300">
+                    <input type="text"
+                           id="name"
+                           name="name"
+                           value={name}
+                           onChange={(e) => setName(e.target.value)}
+                           placeholder="Name"
+                           className="w-full py-2 pl-3 text-sm rounded-md focus:outline-none dark:bg-gray-100 dark:text-gray-800 focus:dark:bg-gray-50 focus:dark:border-blue-600" />
+                </div>
+                <div className="relative rounded-md border border-gray-300">
+                    <input type="text"
+                           id="quantity"
+                           name="quantity"
+                           value={quantity}
+                           onChange={(e) => setQuantity(e.target.value)}
+                           placeholder="Quantity"
+                           className="w-full py-2 pl-3 text-sm rounded-md focus:outline-none dark:bg-gray-100 dark:text-gray-800 focus:dark:bg-gray-50 focus:dark:border-blue-600" />
+                </div>
+                <div className="relative rounded-md border border-gray-300">
+                    <select id="section"
+                            name="section"
+                            value={section}
+                            onChange={(e) => setSection(e.target.value as Section)}
+                            className="w-full p-2 text-sm rounded-md focus:outline-none dark:bg-gray-100 dark:text-gray-800 focus:dark:bg-gray-50 focus:dark:border-blue-600">
                         {sections.map((section, index) => (<option key={index} value={section}>{section}</option>))}
-                </select>
-                <button type="submit">Add</button>
-            </form>
-
-        </>
-    )
+                    </select>
+                </div>
+                <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded-md">Add</button>
+            </fieldset>
+        </form>
+    );
 }
