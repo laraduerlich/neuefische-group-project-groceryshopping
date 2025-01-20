0aa9c2d2af03e4d050e625e2b4b2fdbc835cc7f7
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-01-20 12:37:28 +0100

    feat: Append the item to a state in the list when adding it

--- frontend/src/pages/ShoppingListPage.tsx
+++ frontend/src/pages/ShoppingListPage.tsx
@@ -1,17 +1,21 @@
 import ItemForm from "../components/ItemForm.tsx";
 import {Item} from "../type/Item.tsx";
+import {useState} from "react";
 
 export default function ShoppingListPage(){
 
-    const handleSubmit = (item: Item, quantity: string) => {
-        console.log("Form submitted:", item, quantity);
-        // Hier können Sie die Daten weiterverarbeiten, z. B. an einen Server senden
+    const [items, setItems] = useState<{item: Item, quantity: string}[]>([]);
+
+    const handleSubmitNewItem = (item: Item, quantity: string) => {
+
+        setItems([...items, {item, quantity}]);
+        console.log(items);
     };
 
     return (
         <div className="shopping-list-page">
             Shopping List Page
-            <ItemForm onSubmit={handleSubmit} // onSubmit-Handler übergeben
+            <ItemForm onSubmit={handleSubmitNewItem}
             />
         </div>
     )
