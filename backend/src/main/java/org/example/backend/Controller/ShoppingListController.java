13ec174bda0c7daf8d42b2aff7b8f22c14ad4540
Coralee Long <coraleelong@gmail.com>
2025-01-21 12:23:56 +0100

    remove item repo and related code

--- backend/src/main/java/org/example/backend/Controller/ShoppingListController.java
+++ backend/src/main/java/org/example/backend/Controller/ShoppingListController.java
@@ -1,8 +1,6 @@
 package org.example.backend.Controller;
 
-import org.example.backend.Model.Item;
 import org.example.backend.Model.ShoppingList;
-import org.example.backend.Service.ItemService;
 import org.example.backend.Service.ShoppingListService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,24 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/shoppinglists")
 public class ShoppingListController {
 
     private final ShoppingListService shoppingListService;
-    private final ItemService itemService;
 
-    public ShoppingListController(ShoppingListService shoppingListService, ItemService itemService) { // Include ItemService
+    public ShoppingListController(ShoppingListService shoppingListService) { // Include ItemService
         this.shoppingListService = shoppingListService;
-        this.itemService = itemService;
     }
 
-    @GetMapping("/shoppinglists")
+    @GetMapping
     public ResponseEntity<List<ShoppingList>> getAllShoppingLists() {
         return ResponseEntity.ok(shoppingListService.getAllShoppingLists());
-    }
-
-    @GetMapping("/items")
-    public ResponseEntity<List<Item>> getAllItems() {
-        return ResponseEntity.ok(itemService.getAllItems());
     }
 }
--- backend/src/main/java/org/example/backend/Model/Item.java
+++ backend/src/main/java/org/example/backend/Model/Item.java
@@ -1,8 +1,6 @@
 package org.example.backend.Model;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Items")
 public record Item(
     @Id String id,
     String name,
