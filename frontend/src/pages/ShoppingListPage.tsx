62c0f7ecfd35c1e25d78d5759d8f4c4e7db8dc64
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-01-22 15:54:10 +0100

    feat: Add button to go shopping when creating a new list
    The button will later send the shopping list to the backend and
    get the id, before navigating to the corresponding shopping page

--- frontend/src/pages/ShoppingListPage.tsx
+++ frontend/src/pages/ShoppingListPage.tsx
@@ -2,13 +2,20 @@ import ItemForm from "../components/ItemForm.tsx";
 import {Item} from "../type/Item.tsx";
 import {useState} from "react";
 import GroupedItems from "../components/GroupedItems.tsx";
+import {useNavigate} from "react-router-dom";
 
 export default function ShoppingListPage(){
 
     const [items, setItems] = useState<{item: Item, quantity: string}[]>([]);
 
-    const handleSubmitNewItem = (item: Item, quantity: string) => {
+    const handleGoShoppingButtonClick = () => {
+        const id: string = ""  // Get this from the POST API call
+        navigate("/shopping/" + id)
+    };
 
+    const navigate = useNavigate();
+
+    const handleSubmitNewItem = (item: Item, quantity: string) => {
         setItems([...items, {item, quantity}]);
         console.log(items);
     };
@@ -19,6 +26,12 @@ export default function ShoppingListPage(){
             <ItemForm onSubmit={handleSubmitNewItem}
             />
             <GroupedItems items={items} checkbox={false}/>
+            <button
+                onClick={() => handleGoShoppingButtonClick()}
+                className="px-4 py-2 text-sm text-white bg-blue-500 rounded-md hover:bg-blue-600"
+            >
+                Save and go 🛒
+            </button>
         </div>
     );
 
