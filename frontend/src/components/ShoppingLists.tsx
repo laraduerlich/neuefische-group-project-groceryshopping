ec6d74a91035885870a71114d9b7e5e79fefdc1f
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-01-23 11:26:11 +0100

    fix: Mark lists as optional
    Co-authored-by: Coralee <45268389+Coralee-Long@users.noreply.github.com>

--- frontend/src/components/ShoppingLists.tsx
+++ frontend/src/components/ShoppingLists.tsx
@@ -2,7 +2,7 @@ import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 
 type ShoppingListsProps = {
-    lists: {
+    lists?: {
         id: string,
         name: string
     }[],
