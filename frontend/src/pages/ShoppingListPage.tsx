f375ade8d205dca474a1fde7f4342586b72329a4
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-01-23 11:47:01 +0100

    feat: Add navigation buttons to the shopping list creation page (#39)
    Two buttons are added to the shopping list creation page:
    
    - one to save the list and go back to the home
    - one to save the list and go to the shopping page
    
    ---------                                                                                                                                                                                                                              
                                                                                                                                                                                                                                                                                                                                                                                                   Co-authored-by: Coralee <45268389+Coralee-Long@users.noreply.github.com>
    Co-authored-by: laraduerlich <lara.duerlich@t-online.com>

--- frontend/src/pages/ShoppingListPage.tsx
+++ frontend/src/pages/ShoppingListPage.tsx
@@ -1,24 +1,64 @@
 import ItemForm from "../components/ItemForm.tsx";
 import {Item} from "../type/Item.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import GroupedItems from "../components/GroupedItems.tsx";
+import {useNavigate} from "react-router-dom";
 
 export default function ShoppingListPage(){
 
     const [items, setItems] = useState<{item: Item, quantity: string}[]>([]);
 
-    const handleSubmitNewItem = (item: Item, quantity: string) => {
+    const [shoppingListName, setShoppingListName] = useState("");
 
-        setItems([...items, {item, quantity}]);
-        console.log(items);
+    const navigate = useNavigate();
+
+    const handleGoShoppingButtonClick = () => {
+        const id: string = ""  // Get this from the POST API call
+        navigate("/shopping/" + id)
     };
+
+    const handleSaveAndGoHomeButtonClick = () => {
+        // Add POST API call here
+        navigate("/")
+    };
+
+    const handleSubmitNewItem = (item: Item, quantity: string) => {
+        setItems([...items, {item, quantity}]);
+    };
+
+    useEffect(() => {
+        console.log(items);
+    }, [items]);  // every time the state of items change, the items will be logged
 
     return (
         <div className="shopping-list-page">
             Shopping List Page
+            <div className="relative rounded-md border border-gray-300">
+                <input type="text"
+                       id="shoppingListName"
+                       name="shoppingListName"
+                       value={shoppingListName}
+                       onChange={(e) => setShoppingListName(e.target.value)} required
+                       placeholder="Shopping List Name"
+                       className="w-full py-2 pl-3 text-sm rounded-md focus:outline-none dark:bg-gray-100 dark:text-gray-800 focus:dark:bg-gray-50 focus:dark:border-blue-600"/>
+            </div>
             <ItemForm onSubmit={handleSubmitNewItem}
             />
             <GroupedItems items={items} checkbox={false}/>
+            <div className="flex justify-center">
+            <button
+                onClick={() => handleSaveAndGoHomeButtonClick()}
+                className="px-4 py-2 text-sm text-white bg-blue-500 rounded-md hover:bg-blue-600"
+            >
+                Save and go 🏠
+            </button>
+            <button
+                onClick={() => handleGoShoppingButtonClick()}
+                className="ml-4 px-4 py-2 text-sm text-white bg-blue-500 rounded-md hover:bg-blue-600"
+            >
+                Save and go 🛒
+            </button>
+            </div>
         </div>
     );
 
